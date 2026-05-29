d1d4957fda909700e8a84d39f242a08fefcf7888
Севостьянов Егор <egodmisevostyanov@edu.hse.ru>
2026-05-29 22:42:28 +0300

    Реализован весь функционал класса Recipe: __init__, методы add_ingredient и scale, статический метод is_valid_ratio и методы __len__ и __str__

--- hw2.py
+++ hw2.py
@@ -24,3 +24,34 @@ class Ingredient:
         if not isinstance(other, Ingredient):
             return NotImplemented
         return (self.name == other.name and self.unit == other.unit)
+
+
+class Recipe:
+    def __init__(self, title : str, ingredients : list):
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient : Ingredient):
+        for i in self.ingredients:
+            if i == ingredient:
+                i.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+    
+    def scale(self, ratio : float):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("ratio должен быть числом больше нуля")
+        ing = []
+        for i in self.ingredients:
+            ing.append(Ingredient(i.name, i.quantity * ratio, i.unit))
+        return Recipe(self.title, ing)
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        return f"{self.title}: {', '.join(str(i) for i in self.ingredients)}"
